7bc825ec4b8a3dcac70601846b105936556354b3
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2013-01-21 11:40:35 +0100

    tp2 done

--- bank.py
+++ bank.py
@@ -1,23 +1,52 @@
 class Account(object):
     
-    def __init__(self):
-        self.operations = list()
+    def __init__(self, storage):
+        self.operations = storage
         
     def historique(self):
-        return list(self.operations)
+        return list(self.operations.select())
             
     def balance(self):
-        return sum(self.operations)
+        """Retourne le solde courant du compte
+        """
+        return sum(self.operations.select())
+        
+    def _negative_value_raises_an_error(self,value):    
+        if not self.can_accept_credit(value):
+            raise ValueError("The value should be positive. You have provided {0}".format(value))    
         
     def credit(self, value):
-        if value < 0:
-            raise ValueError("The value should be positive. You have provided {0}".format(value))
-        self.operations.append(value);
+        """Credite le compte courant de value
+        
+        ValueError si value est negatif
+        """
+        self._negative_value_raises_an_error(value)
+        self.operations.insert(value);
         
     def debit(self, value):
-        if value < 0:
-            raise ValueError("The value should be positive. You have provided {0}".format(value))
-        if self.balance() < 0:
-            raise ValueError("Balance negative")
-        self.operations.append(-value);
+        """Debite le compte courant de value
         
+        ValueError si value est negatif
+        """
+        self._negative_value_raises_an_error(value)
+        current_balance = self.balance()
+        if self.balance() < value:
+            raise ValueError("Value os bigger than the current balance")
+        self.operations.insert(-value);
+        
+    def can_accept_credit(self, value):
+        """Retourne vrai si j'autorise un credit de value"""
+        return value >= 0
+        
+class Transfert(object):
+    """Class managing transfers between two accounts."""
+    def __init__(self, src, dest):
+        self.source = src
+        self.destination = dest
+        
+    def transfer(self, amount):
+        """Transfer amount from source to destination"""
+        if not self.destination.can_accept_credit(amount):
+            raise ValueError("Destination account can not accept a credit of {0}".format(amount))
+        self.source.debit(amount)
+        self.destination.credit(amount)
--- test_bank.py
+++ test_bank.py
@@ -1,58 +1,133 @@
 import unittest
-import doctest
 
 from bank import Account
+from bank import Transfert
 
-class TestJohnOuvreUnCompte(unittest.TestCase):
-    """
-    Cette classe test l'ouverture d'un compte ainsi que les deux operations elementaires que sont
-    les credits et les debits
-    """
+from mockito import mock
+from mockito import verify
+from mockito import when
+from mockito import never
+from mockito import times
+
     
-    def test_ouverture_de_compte_solde_a_zero(self):
-        """Test l'ouverture d'une compte et son solde"""
-
-        account = Account()
-        self.assertEquals(account.balance(), 0)
+class TestJohnCrediteSonCompte(unittest.TestCase):
         
     def test_credit_du_compte_positivement(self):
         """Test l'accreditement d'un compte"""
-        
-        account = Account()
+        storage = mock()
+        account = Account(storage)
         amount = 100
-        old_balance = account.balance()
         account.credit(amount)
-        new_balance = account.balance()
-        self.assertEquals(old_balance+amount, new_balance)
-        
+        verify(storage).insert(amount)
+    
     def test_credit_du_compte_negativement(self):
         """Test l'accreditement d'un compte negativement"""
         
-        account = Account()
+        storage = mock()
+        account = Account(storage)
         amount = -10
         self.assertRaises(ValueError, account.credit, amount)
+
+class TestJohnDebiteSonCompte(unittest.TestCase):
         
     def test_debit_du_compte_positivement(self):
         """Test le debit d'un compte avec une valeur positive"""
         
-        account = Account()
+        storage = mock()
+        when(storage).select().thenReturn([105])
+        account = Account(storage)
         amount = 100
-        old_balance = account.balance()
         account.debit(amount)
-        new_balance = account.balance()
-        self.assertEquals(old_balance-amount, new_balance)
+        verify(storage).insert(-amount)
         
-    def test_debit_sur_solde_negatif(self): 
+    def _test_debit_sur_solde_negatif(self): 
         """Test le debit quand le solde est deja negatif""" 
-        account = Account()
+        storage = mock()
+        account = Account(storage)
         amount = -100
         self.assertRaises(ValueError, account.debit, amount)
         
     def test_debit_du_compte_negativement(self):
         """Test le debit d'un compte negativement"""
         
-        account = Account()
+        storage = mock()
+        account = Account(storage)
         amount = -10
         self.assertRaises(ValueError, account.debit, amount)
         
+    def test_debit_d_une_valeur_trop_importante(self):
+        """Test le debit par une valeur trop importante aka plus grande que le solde courant"""
+        storage = mock()
+        when(storage).select().thenReturn([5])
+        account = Account(storage)
+        amount = 10
         
+class TestJohnOuvreUnCompte(unittest.TestCase):
+    """
+    Cette classe test l'ouverture d'un compte ainsi que les deux operations elementaires que sont
+    les credits et les debits
+    """
+    def setUp(self):
+        self.storage = mock()
+        self.account = Account(self.storage)
+    
+    def test_ouverture_de_compte_solde_a_zero(self):
+        """Test l'ouverture d'une compte et son solde"""
+        
+        when(self.storage).select().thenReturn([])
+        self.assertEquals(self.account.balance(), 0)
+        
+    def test_balance(self):
+        """Test la balance d'un compte"""
+        when(self.storage).select().thenReturn([10, -5])
+        balance = self.account.balance()
+        self.assertEquals(balance, 5)    
+        
+class TestJohnTrasnfertDeLArgentASaFemme(unittest.TestCase):
+    
+    def setUp(self):
+        self.accountSrc = mock()
+        self.accountDest = mock()
+        self.transfer = Transfert(self.accountSrc, self.accountDest)
+    
+    def test_transfert_debite_le_bon_compte(self):
+        """Test si lors du transfert la valeur est bien debitee de la source"""
+
+        amount = 100
+        when(self.accountDest).can_accept_credit(amount).thenReturn(True)
+        self.transfer.transfer(amount)
+        verify(self.accountSrc, times=1).debit(amount)
+        
+    def test_transfert_credite_le_bon_compte(self):
+        """Test si lors du transfert la valeur est bien creditee a la destination"""
+      
+        amount = 100
+        when(self.accountDest).can_accept_credit(amount).thenReturn(True)
+        self.transfer.transfer(amount)
+        verify(self.accountDest, times=1).credit(amount)
+        
+    def test_transfert_credite_et_debite_de_la_meme_valeur(self):  
+        """Test que lors d'un transfert la valeur debitee et creditee est la meme"""  
+        amount = 100
+        when(self.accountDest).can_accept_credit(amount).thenReturn(True)
+        self.transfer.transfer(amount)
+        verify(self.accountDest, times=1).credit(amount)
+        verify(self.accountSrc, times=1).debit(amount)
+
+    def test_debit_errone_ne_credite_pas(self):
+        """Test si en cas d'erreur lors du debit, il n'y a pas de credit"""
+        
+        amount = 100
+        when(self.accountDest).can_accept_credit(amount).thenReturn(True)
+        when(self.accountSrc).debit(amount).thenRaise(ValueError)
+        
+        self.assertRaises(ValueError, self.transfer.transfer, amount)
+        verify(self.accountDest, never).credit(amount)
+        
+    def test_credit_errone_ne_debite_pas(self):
+        """Test si quand le credit n'est pas possible, il n'y a pas de debit"""
+        
+        amount = 100
+        when(self.accountDest).can_accept_credit(amount).thenReturn(False)
+        self.assertRaises(ValueError, self.transfer.transfer, amount)
+        verify(self.accountSrc, never).debit(amount)    
